dac286a6b834c27c0ca6e26532cc3461f6c39288
nadeemmeerza <41850100+nadeemmeerza@users.noreply.github.com>
2023-04-12 17:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-	let str2 = str.reverse()
+	let str2 = str.split('').reverse().join('');
 	if(str2 === str)
 		return true;
 	return false;
